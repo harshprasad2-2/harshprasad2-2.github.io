a279d9a4d3993d03e0d3b38d35918c812c93283f
harshprasad2-2 <95679131+harshprasad2-2@users.noreply.github.com>
2022-07-28 15:28:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// var NavList = document.getElementsByClassName('nav');
+// var toggle = document.getElementsByClassName('nav-logo-link');
+// function myFunction() {
+//     var x = document.getElementsByClassName("nav-logo-link");
+//     if (x.style.display === "none") {
+//       x.style.display = "flex";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
